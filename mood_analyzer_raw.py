5513190f08891819f5b0b025bb626c7de6d5163f
7even-13 <leesha0613@gmail.com>
2025-06-22 11:33:20 +0530

    Create mood_analyzer_raw.py
    🖤Turned fire into fuel. Mood Analyzer now speaks your truth.

---
+++ mood_analyzer_raw.py
@@ -0,0 +1,13 @@
+mood = input(f"What's your soul status rn? ").lower()
+if mood == "happy":
+    print(f"Hapiness looks good on you. It's not permenant but if you cherish yourself, it'll remain a little longer.")
+if mood == "sad":
+    print(f"It's normal to feel sadness bcz you're only human.Let it all out,leave all your sadness and regrets along with the sunset and let the moon witness a new start. ")
+if mood == "angry":
+    print(f"I see that you're about to explode, but hold that fire in.Use your fire as fuel,not a weapon.")
+elif mood == "tired":
+    print(f"This battle ain't over.You're just sharpening ur sword so you can slay the next day instead of just 'surviving'.")
+elif mood == "confused":
+    print(f"Being lost is a part of being alive.It's only a temporary crisis.You'll eventually find ur way back to urself.")
+elif mood == "curious":
+    print(f"You're lucky to be born with a whole constellation inside ur soul unlike most people; who are empty souls.Channel ur curiosity with creativity,you're meant to make the world a better place.")
